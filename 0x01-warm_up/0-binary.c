598533015ede5a93520edfa90e9d856a3af65b37
Rd-Feng <394@holbertonschool.com>
2019-03-25 18:09:05 -0700

    init commit

---
+++ 0x01-warm_up/0-binary.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+void print_array_bs(int *array, int l, int r);
+/**
+ * binary_search - binary search
+ * @array: array to search
+ * @size: size of array
+ * @value: value to search for
+ *
+ * Return: target index, -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int l = 0, r = size - 1, n;
+
+	while (array && l <= r)
+	{
+		print_array_bs(array, l, r);
+		n = (l + r) / 2;
+		if (array[n] == value)
+			return (n);
+		else if (array[n] < value)
+			l = n + 1;
+		else
+			r = n - 1;
+	}
+	return (-1);
+}
+/**
+ * print_array_bs - print array for binary search
+ * @array: array to print
+ * @l: left bound
+ * @r: right bound
+ */
+void print_array_bs(int *array, int l, int r)
+{
+	printf("Searching in array: ");
+	while (l < r)
+		printf("%d, ", array[l++]);
+	printf("%d\n", array[r]);
+}
